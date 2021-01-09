1fba57ccc78661229c5d6fbfefa17d1105405dc0
garden627 <garden627@yahoo.co.jp>
2021-01-09 23:09:50 +0900

    test

--- btre/settings.py
+++ btre/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = '^+nz-4)f8fcl%jsn7v+1k@l*o!gt#b#zn1z@gdpd8(jmkqrdx4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS = ['guarded-beach-96655', 'localhost']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['guarded-beach-96655.herokuapp.com', 'localhost']
 # ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
