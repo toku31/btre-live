c60f61623623056a010b4a1b3ae868d973482e67
garden627 <garden627@yahoo.co.jp>
2022-10-29 13:06:51 +0900

    urls

--- btre/urls.py
+++ btre/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve  #追加
 
 urlpatterns = [
     path('', include('pages.urls')),
@@ -9,4 +10,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
+    path('media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
