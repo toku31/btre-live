1fdf773b528e331a419a3176d6214487732116d6
garden627 <garden627@yahoo.co.jp>
2022-10-30 10:38:59 +0900

    re_path

--- btre/urls.py
+++ btre/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-# from django.views.static import serve  #追加
+from django.views.static import serve  #追加
 
 urlpatterns = [
     path('', include('pages.urls')),
@@ -11,6 +11,8 @@ urlpatterns = [
     path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
     # path('media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}), 
+    
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
