d6534981b634aa53164ffb4da516087089f6e714
garden627 <garden627@yahoo.co.jp>
2022-10-30 10:42:26 +0900

    added

--- btre/urls.py
+++ btre/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}), 
     
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
